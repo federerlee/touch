484fc1f093d1663db7a9382603b81d4229561cf9
adamp <adamp@android.com>
2010-07-11 23:17:57 +0000

    Cupcake compatibility code example.

--- src/com/example/android/touchexample/TouchExampleView.java
+++ src/com/example/android/touchexample/TouchExampleView.java
@@ -20,21 +20,14 @@ import android.graphics.Canvas;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
-import android.view.ScaleGestureDetector;
 import android.view.View;
 
 public class TouchExampleView extends View {
-    private static final int INVALID_POINTER_ID = -1;
-    
     private Drawable mIcon;
     private float mPosX;
     private float mPosY;
     
-    private float mLastTouchX;
-    private float mLastTouchY;
-    private int mActivePointerId = INVALID_POINTER_ID;
-    
-    private ScaleGestureDetector mScaleDetector;
+    private VersionedGestureDetector mDetector;
     private float mScaleFactor = 1.f;
     
     public TouchExampleView(Context context) {
@@ -50,74 +43,12 @@ public class TouchExampleView extends View {
         mIcon = context.getResources().getDrawable(R.drawable.icon);
         mIcon.setBounds(0, 0, mIcon.getIntrinsicWidth(), mIcon.getIntrinsicHeight());
         
-        mScaleDetector = new ScaleGestureDetector(context, new ScaleListener());
+        mDetector = VersionedGestureDetector.newInstance(context, new GestureCallback());
     }
     
     @Override
     public boolean onTouchEvent(MotionEvent ev) {
-        // Let the ScaleGestureDetector inspect all events.
-        mScaleDetector.onTouchEvent(ev);
-        
-        final int action = ev.getAction();
-        switch (action & MotionEvent.ACTION_MASK) {
-        case MotionEvent.ACTION_DOWN: {
-            final float x = ev.getX();
-            final float y = ev.getY();
-            
-            mLastTouchX = x;
-            mLastTouchY = y;
-            mActivePointerId = ev.getPointerId(0);
-            break;
-        }
-            
-        case MotionEvent.ACTION_MOVE: {
-            final int pointerIndex = ev.findPointerIndex(mActivePointerId);
-            final float x = ev.getX(pointerIndex);
-            final float y = ev.getY(pointerIndex);
-
-            // Only move if the ScaleGestureDetector isn't processing a gesture.
-            if (!mScaleDetector.isInProgress()) {
-                final float dx = x - mLastTouchX;
-                final float dy = y - mLastTouchY;
-
-                mPosX += dx;
-                mPosY += dy;
-
-                invalidate();
-            }
-
-            mLastTouchX = x;
-            mLastTouchY = y;
-
-            break;
-        }
-            
-        case MotionEvent.ACTION_UP: {
-            mActivePointerId = INVALID_POINTER_ID;
-            break;
-        }
-            
-        case MotionEvent.ACTION_CANCEL: {
-            mActivePointerId = INVALID_POINTER_ID;
-            break;
-        }
-        
-        case MotionEvent.ACTION_POINTER_UP: {
-            final int pointerIndex = (ev.getAction() & MotionEvent.ACTION_POINTER_INDEX_MASK) 
-                    >> MotionEvent.ACTION_POINTER_INDEX_SHIFT;
-            final int pointerId = ev.getPointerId(pointerIndex);
-            if (pointerId == mActivePointerId) {
-                // This was our active pointer going up. Choose a new
-                // active pointer and adjust accordingly.
-                final int newPointerIndex = pointerIndex == 0 ? 1 : 0;
-                mLastTouchX = ev.getX(newPointerIndex);
-                mLastTouchY = ev.getY(newPointerIndex);
-                mActivePointerId = ev.getPointerId(newPointerIndex);
-            }
-            break;
-        }
-        }
-        
+        mDetector.onTouchEvent(ev);
         return true;
     }
 
@@ -132,16 +63,20 @@ public class TouchExampleView extends View {
         canvas.restore();
     }
 
-    private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
-        @Override
-        public boolean onScale(ScaleGestureDetector detector) {
-            mScaleFactor *= detector.getScaleFactor();
+    private class GestureCallback implements VersionedGestureDetector.OnGestureListener {
+        public void onDrag(float dx, float dy) {
+            mPosX += dx;
+            mPosY += dy;
+            invalidate();
+        }
+
+        public void onScale(float scaleFactor) {
+            mScaleFactor *= scaleFactor;
             
             // Don't let the object get too small or too large.
             mScaleFactor = Math.max(0.1f, Math.min(mScaleFactor, 5.0f));
 
             invalidate();
-            return true;
         }
     }
 }
